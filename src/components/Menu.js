f92fd2be60ec2561a9b8c4b2d1bad6c9d7444f46
emreavcia <avciemrea@gmail.com>
2023-05-22 17:31:21 +0200

    added some changes

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,17 +1,22 @@
 import { Link } from "react-router-dom";
 
+import "../css/menu.css";
+
 const Menu = () => {
   return (
     <header>
-      <nav>
-        <ul>
-          <li>
+      <Link to="/" className="logo">
+        EA
+      </Link>
+      <nav class="navigation">
+        <ul class="navigation__list">
+          <li class="navigation__list__item">
             <Link to="/">HOME</Link>
           </li>
-          <li>
+          <li class="navigation__list__item">
             <Link to="/about">ABOUT</Link>
           </li>
-          <li>
+          <li class="navigation__list__item">
             <Link to="/contact">CONTACT</Link>
           </li>
         </ul>
